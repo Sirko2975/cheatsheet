c7e60655a0cb5a2a86af5045379812683b549449
Sirko2975 <121972871+Sirko2975@users.noreply.github.com>
2023-03-08 20:30:47 +0200

    Add files via upload

---
+++ cs.py
@@ -0,0 +1,11 @@
+print ('\033[1m' + '\nCheatesheet v1.0\n' + '\033[0m')
+
+print ("arp -a                   |  Scan local network")
+print ("Ifconfig                 |  Wireless info")
+print ("Sudo shutdown ___ now    |  Instant shutdown")
+print ("uptime                   |  Check session time")
+print ("caffeinate               |  Stay awake")
+print ("alias                    |  Command pseudo")
+print ("bind                     |  Display readline key and function bindings")
+print ("halt                     |  Stop and reboot")
+print (" ")
